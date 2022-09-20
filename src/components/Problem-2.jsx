f19a6adf8324a640e2a7eb5a2da41a5123d86ada
mynul <homeexercise8@gmail.com>
2022-09-20 20:43:20 +0600

    modal done

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
-
+import { useState } from 'react';
+import '../assets/scss/PopUp/popup.scss'
 const Problem2 = () => {
-
+    const [ModalA, setModalA] = useState(false)
     return (
 
         <div className="container">
-            <div className="row justify-content-center mt-5">
+            <div className="row justify-content-center mt-5" style={{ zIndex: -1 }}>
                 <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#AllContactModal" className="btn btn-lg btn-outline-primary" >All Contacts</button>
+                    <button className="btn btn-lg btn-outline-warning" type="button" data-bs-toggle="modal" data-bs-target="#UsContactModal">US Contacts</button>
                 </div>
-                
+
+            </div>
+            <div class="modal fade" id="AllContactModal"   >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <div class="modal-body">
+                            hello
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="UsContactModal"   >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+
+                        <div class="modal-body">
+                            hello
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     );
